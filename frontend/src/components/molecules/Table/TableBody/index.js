d92c8621b32a6871b978896172e8ef4c20a77e0e
Fernando Gontijo <fernandolucasbd@gmail.com>
2019-03-30 16:33:33 -0300

    removendo console.log

--- frontend/src/components/molecules/Table/TableBody/index.js
+++ frontend/src/components/molecules/Table/TableBody/index.js
@@ -107,7 +107,6 @@ const publishDeleteItem = (mutation, item, refetchRows, string) => async () => {
       refetchRows()
     }
   } catch (error) {
-    console.log(error)
     Alert('error', `Error', 'Erro ao ${string} item!`)
   }
 }
