4135abcd77c69cef493e1ef87554e0b1729c9f5d
Fernando Gontijo <fernandolucasbd@gmail.com>
2019-05-12 18:49:53 -0300

    abstraindo função de login para Command do cypress

--- E2E/cypress/support/commands.js
+++ E2E/cypress/support/commands.js
@@ -1,25 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (email = 'fernando@gmail.com', password = "%fernando%123") => {
+  cy.visit('http://localhost:3000/auth/login')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password)
+  cy.get('#show-password').click()
+  cy.get('#login').click()
+  cy.wait(5000)
+})
